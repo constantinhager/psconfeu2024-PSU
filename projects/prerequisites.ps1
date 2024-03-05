2f1a9e008515492df2b4e1b1e9627a7dd9e8737f
Constantin Hager <constantin.hager@outlook.de>
2024-03-05 19:57:01 +0000

    added prerequisites file

---
+++ projects/prerequisites.ps1
@@ -0,0 +1,10 @@
+wsl --install
+
+winget install Microsoft.PowerShell
+winget install Microsoft.VisualStudioCode.Insiders
+winget install Git.Git
+winget install Docker.DockerDesktop
+
+# configure git
+git config --global user.name "<your name>"
+git config --global user.email "<your email>"
